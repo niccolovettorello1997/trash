b28c9200f176a5857fc2227cb2d61a9c69e52fc3
vettorelloniccolo97 <vettorelloniccolo97@gmail.com>
2024-01-13 15:08:31 +0100

    first shape for parser

---
+++ src/scripts/parser.js
@@ -0,0 +1,40 @@
+/**
+ * Given data and a date, returns an array of trash.
+ * 
+ * @param   string data
+ * @param   number year
+ * @param   number month
+ * @param   number day
+ * @returns Array
+ */
+function parse(data, year, month, day)
+{
+
+}
+
+/**
+ * Returns false if there are no trashes for this date.
+ * 
+ * @param   number year
+ * @param   number month
+ * @param   number day
+ * @returns Boolean
+ */
+function preprocessDate(year, month, day)
+{
+
+}
+
+/**
+ * Execute parsing operations.
+ * 
+ * @param   string data
+ * @param   number year
+ * @param   number month
+ * @param   number day
+ * @returns Array
+ */
+function execute(data, year, month, day)
+{
+
+}
